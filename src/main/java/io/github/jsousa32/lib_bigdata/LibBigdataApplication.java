1b4677fb5128cd68feb618ce28a175b8af463dfe
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-10-04 14:38:38 -0300

    first commit

---
+++ src/main/java/io/github/jsousa32/lib_bigdata/LibBigdataApplication.java
@@ -0,0 +1,13 @@
+package io.github.jsousa32.lib_bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibBigdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibBigdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jsousa32/lib_bigdata/LibBigdataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jsousa32.lib_bigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibBigdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
