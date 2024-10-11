767b9582ef27b991f1783b66599ca36a63cd4f62
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-10-11 10:07:34 -0300

    [FEAT]: implementação da estrutura de pessoa

---
+++ src/main/java/io/github/jsousa32/lib_bigdata/people/controls/DefaultPersonService.java
@@ -0,0 +1,34 @@
+package io.github.jsousa32.lib_bigdata.people.controls;
+
+import io.github.jsousa32.lib_bigdata.companies.entities.enums.Scopes;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * The Class DefaultPersonService
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 11/10/2024
+ */
+public class DefaultPersonService implements PersonService {
+
+    private final UriComponentsBuilder uri;
+
+    private final HttpEntity<String> httpEntity;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public DefaultPersonService(UriComponentsBuilder uri, HttpEntity<String> httpEntity) {
+        this.uri = uri;
+        this.httpEntity = httpEntity;
+    }
+
+    private <T> T execute(String document, Scopes scopes, Class<T> responseType) {
+        this.uri.queryParam("document", document);
+        this.uri.path(scopes.getLabel());
+
+        return restTemplate.exchange(this.uri.toUriString(), HttpMethod.GET, this.httpEntity, responseType).getBody();
+    }
+}
---
+++ src/main/java/io/github/jsousa32/lib_bigdata/people/controls/PersonService.java
@@ -0,0 +1,10 @@
+package io.github.jsousa32.lib_bigdata.people.controls;
+
+/**
+ * The Interface PersonService
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 11/10/2024
+ */
+public interface PersonService {
+}
