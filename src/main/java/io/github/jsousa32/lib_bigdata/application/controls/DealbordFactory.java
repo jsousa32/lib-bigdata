fd02b8889f24d8e9af841f1693b7c0238297ffc2
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-10-17 15:46:14 -0300

    [FEAT]: implementação da criação no factory

--- src/main/java/io/github/jsousa32/lib_bigdata/application/controls/DealbordFactory.java
+++ src/main/java/io/github/jsousa32/lib_bigdata/application/controls/DealbordFactory.java
@@ -3,6 +3,8 @@ package io.github.jsousa32.lib_bigdata.application.controls;
 import io.github.jsousa32.lib_bigdata.addresses.controls.AddressService;
 import io.github.jsousa32.lib_bigdata.companies.controls.CompanyService;
 import io.github.jsousa32.lib_bigdata.custom.controls.CustomService;
+import io.github.jsousa32.lib_bigdata.custom.entities.CompanyLegal;
+import io.github.jsousa32.lib_bigdata.custom.entities.CompanyNatural;
 import io.github.jsousa32.lib_bigdata.people.controls.PersonService;
 
 public interface DealbordFactory {
@@ -14,4 +16,8 @@ public interface DealbordFactory {
     AddressService address();
 
     CustomService custom();
+
+    void create(CompanyLegal companyLegal);
+
+    void create(CompanyNatural companyNatural);
 }
--- src/main/java/io/github/jsousa32/lib_bigdata/application/controls/DefaultDealboardFactory.java
+++ src/main/java/io/github/jsousa32/lib_bigdata/application/controls/DefaultDealboardFactory.java
@@ -2,21 +2,31 @@ package io.github.jsousa32.lib_bigdata.application.controls;
 
 import io.github.jsousa32.lib_bigdata.addresses.controls.AddressService;
 import io.github.jsousa32.lib_bigdata.addresses.controls.DefaultAddressService;
+import io.github.jsousa32.lib_bigdata.application.entities.enums.IntegrationScope;
+import io.github.jsousa32.lib_bigdata.application.entities.enums.Scope;
 import io.github.jsousa32.lib_bigdata.companies.controls.CompanyService;
 import io.github.jsousa32.lib_bigdata.companies.controls.DefaultCompanyService;
 import io.github.jsousa32.lib_bigdata.custom.controls.CustomService;
 import io.github.jsousa32.lib_bigdata.custom.controls.DefaultCustomService;
+import io.github.jsousa32.lib_bigdata.custom.entities.CompanyLegal;
+import io.github.jsousa32.lib_bigdata.custom.entities.CompanyNatural;
 import io.github.jsousa32.lib_bigdata.people.controls.DefaultPersonService;
 import io.github.jsousa32.lib_bigdata.people.controls.PersonService;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class DefaultDealboardFactory implements DealbordFactory {
 
+    private final static String PATH_BASE = "/api/v1/integration/";
+
     private final UriComponentsBuilder uri;
 
     private final HttpEntity<String> httpEntity;
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     public DefaultDealboardFactory(UriComponentsBuilder uri, HttpHeaders headers) {
         this.uri = uri;
@@ -41,5 +51,20 @@ public class DefaultDealboardFactory implements DealbordFactory {
     @Override
     public CustomService custom() {
         return new DefaultCustomService(this.uri, this.httpEntity);
+    }
+
+    @Override
+    public void create(CompanyLegal companyLegal) {
+        restTemplate.exchange(uri(CompanyLegal.class), HttpMethod.POST, this.httpEntity, Void.class, companyLegal);
+    }
+
+    @Override
+    public void create(CompanyNatural companyNatural) {
+        restTemplate.exchange(uri(CompanyNatural.class), HttpMethod.POST, this.httpEntity, Void.class, companyNatural);
+    }
+
+    private String uri(Class<?> clazz) {
+        return uri.replacePath(PATH_BASE).replaceQuery(null).pathSegment(Scope.COMPANIES.getLabel()).pathSegment(IntegrationScope.getSegment(clazz))
+                .toUriString();
     }
 }
