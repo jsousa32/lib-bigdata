208757fdb7808844cc61132ca8063055add13625
João Lucas <joao.silva@gfinnovation.com.br>
2025-04-04 17:59:40 -0300

    [FIX]: correção dos datasets

--- src/main/java/io/github/jsousa32/lib_bigdata/application/entities/enums/Dataset.java
+++ src/main/java/io/github/jsousa32/lib_bigdata/application/entities/enums/Dataset.java
@@ -2,39 +2,39 @@ package io.github.jsousa32.lib_bigdata.application.entities.enums;
 
 public enum Dataset {
 
-    REGISTRATION("registration_data"),
+    REGISTRATION("registration-data"),
 
-    BASIC_DATA("basic_data"),
+    BASIC_DATA("basic-data"),
 
-    ECONOMIC_GROUP_KYC("economic_group_kyc"),
+    ECONOMIC_GROUP_KYC("economic-group-kyc"),
 
-    EMPLOYEES_KYC("employees_kyc"),
+    EMPLOYEES_KYC("employees-kyc"),
 
     KYC("kyc"),
 
-    LAWSUITS_DISTRIBUTION_DATA("lawsuits_distribution_data"),
+    LAWSUITS_DISTRIBUTION_DATA("lawsuits-distribution-data"),
 
-    OWNERS_LAWSUITS("owners_lawsuits"),
+    OWNERS_LAWSUITS("owners-lawsuits"),
 
-    OWNERS_LAWSUITS_DISTRIBUTION_DATA("owners_lawsuits_distribution_data"),
+    OWNERS_LAWSUITS_DISTRIBUTION_DATA("owners-lawsuits-distribution-data"),
 
     PROCESSES("processes"),
 
     COLLECTIONS("collections"),
 
-    FAMILY_LAWSUITS_DISTRIBUTION_DATA("family_lawsuits_distribution_data"),
+    FAMILY_LAWSUITS_DISTRIBUTION_DATA("family-lawsuits-distribution-data"),
 
-    FINANCIAL_RISK("financial_risk"),
+    FINANCIAL_RISK("financial-risk"),
 
-    FIRST_LEVEL_RELATIVES_LAWSUITS_DATA("first_level_relatives_lawsuit_data"),
+    FIRST_LEVEL_RELATIVES_LAWSUITS_DATA("first-level-relatives-lawsuit-data"),
 
-    GOVERNMENT_DEBTORS("government_debtors"),
+    GOVERNMENT_DEBTORS("government-debtors"),
 
-    INDEBTEDNESS_QUESTION("indebtedness_question"),
+    INDEBTEDNESS_QUESTION("indebtedness-question"),
 
-    OCCUPATION_DATA("occupation_data"),
+    OCCUPATION_DATA("occupation-data"),
 
-    UNIVERSITY_STUNDENT_DATA("university_student_data"),
+    UNIVERSITY_STUNDENT_DATA("university-student-data"),
 
     VEHICLES("vehicles"),
 
