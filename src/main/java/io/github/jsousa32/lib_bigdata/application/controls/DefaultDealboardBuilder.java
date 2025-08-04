cd3b7626b3b50f82e42cac883ce047d4372392e0
João Lucas <joao.silva@gfinnovation.com.br>
2025-08-04 11:44:00 -0300

    [FIX]: remoção da classe principal

--- src/main/java/io/github/jsousa32/lib_bigdata/application/controls/DefaultDealboardBuilder.java
+++ src/main/java/io/github/jsousa32/lib_bigdata/application/controls/DefaultDealboardBuilder.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
+
 final class DefaultDealboardBuilder implements DealboardBuilder.Builder {
 
     private static final String TOKEN = "Token";
